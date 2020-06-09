08783eea5cf24231b7f6f5b76dfdf3d033360840
Abdul Jaseem <jaseemabdul20@gmail.com>
2020-06-09 17:49:16 +0530

    Portfolio page1 with static content

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/assets'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res){
+    res.redirect('/home');
+});
+
+app.get('/home', function(req,res){
+    res.render('index.ejs');
+
+});
+app.get('/index.ejs', function(req,res){
+    res.redirect('/home');
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000);
---
+++ controllers/dbConf.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+    host: '159.65.159.79',
+    user: 'appdotlab',
+    password: 'androidstudio3',
+    database: 'test'
+});
+module.exports = connection;
